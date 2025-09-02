7a5567796d4300c19b7849e38346e2d82e55476c
vijaynvb <vijay.nvb@gmail.com>
2025-09-02 15:37:41 +0530

    express updates

---
+++ nodejs/02_express/ex_02_routes/index.js
@@ -0,0 +1,27 @@
+import express, { json } from "express";
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/todos", (req, res) => {
+    res.json({message: "get all objects"});
+});
+
+app.get("/todos/:id", (req, res) => {
+    res.json({message: "get object by id"});
+});
+
+app.post("/todos", (req, res) => {
+   res.json({message: "object Added"});
+});
+
+app.put("/todos/:id", (req, res) => {
+   res.json({message: "object updated"});
+});
+
+app.delete("/todos/:id", (req, res) => {
+   res.json({message: "object delete"});
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
